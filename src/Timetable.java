e503875dfffdf6057b352de29bbaf096d3b2e778
Chelchock <nigmatzianow.marsel@gmail.com>
2026-05-14 18:09:58 +0300

    feat: реализация расписания (Timetable) - Спринт 6

--- src/Timetable.java
+++ src/Timetable.java
@@ -1,4 +1,9 @@
-import java.util.*;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Timetable {
 
