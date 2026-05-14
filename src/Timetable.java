2d5220c0c38dc398a5cc7d223f1d1b0b1adcdfab
Chelchock <nigmatzianow.marsel@gmail.com>
2026-05-14 20:46:53 +0300

    refactor: уточнил импорты в Timetable

--- src/Timetable.java
+++ src/Timetable.java
@@ -1,4 +1,10 @@
-import java.util.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class Timetable {
 
